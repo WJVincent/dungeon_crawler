a459dd104f90fb9f738109c9834b8336892ad7a0
WJVincent <william@william-vincent.dev>
2023-03-25 00:21:42 -0400

    spawned amulet for victory condition

--- src/components.rs
+++ src/components.rs
@@ -1,19 +1,25 @@
 pub use crate::prelude::*;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Render {
-    pub color: ColorPair,
-    pub glyph: FontCharType,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Player;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Item;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct AmuletOfYala;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct MovingRandomly;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ChasingPlayer;
+
+#[derive(Clone, PartialEq)]
+pub struct Name(pub String);
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToMove {
@@ -22,19 +28,19 @@ pub struct WantsToMove {
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Health {
-    pub current: i32,
-    pub max: i32,
-}
-
-#[derive(Clone, PartialEq)]
-pub struct Name(pub String);
-
-#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToAttack {
     pub attacker: Entity,
     pub victim: Entity,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct ChasingPlayer;
+pub struct Health {
+    pub current: i32,
+    pub max: i32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
