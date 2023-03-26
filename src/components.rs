0e7760e5808b55ded70f4f777546f15d6acf17f2
WJVincent <william@william-vincent.dev>
2023-03-26 16:53:26 -0400

    simple field of view added

--- src/components.rs
+++ src/components.rs
@@ -1,4 +1,5 @@
 pub use crate::prelude::*;
+use std::collections::HashSet;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Player;
@@ -43,4 +44,29 @@ pub struct Health {
 pub struct Render {
     pub color: ColorPair,
     pub glyph: FontCharType,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct FieldOfView {
+    pub visible_tiles: HashSet<Point>,
+    pub radius: i32,
+    pub is_dirty: bool,
+}
+
+impl FieldOfView {
+    pub fn new(radius: i32) -> Self {
+        Self {
+            visible_tiles: HashSet::new(),
+            radius,
+            is_dirty: true,
+        }
+    }
+
+    pub fn clone_dirty(&self) -> Self {
+        Self {
+            visible_tiles: HashSet::new(),
+            radius: self.radius,
+            is_dirty: true,
+        }
+    }
 }
