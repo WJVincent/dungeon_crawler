2dbb963a0d01b592e168c5a1bdcedf06eb476fd9
WJVincent <william@william-vincent.dev>
2023-03-26 17:47:24 -0400

    tiles already visited remain visible

--- src/map.rs
+++ src/map.rs
@@ -9,6 +9,7 @@ pub enum TileType {
 
 pub struct Map {
     pub tiles: Vec<TileType>,
+    pub revealed_tiles: Vec<bool>,
 }
 
 impl BaseMap for Map {
@@ -52,6 +53,7 @@ impl Map {
     pub fn new() -> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
+            revealed_tiles: vec![false; NUM_TILES],
         }
     }
 
@@ -94,5 +96,5 @@ impl Map {
 // y = index / width rounded down
 
 pub fn find_idx(x: i32, y: i32) -> usize {
-    usize::try_from((y * SCREEN_WIDTH) + x).unwrap()
+    ((y * SCREEN_WIDTH) + x) as usize
 }
